7e465342ab2485af768bd133f0c0efd201a2f431
serinc serovine <167237979+Serovine@users.noreply.github.com>
2026-06-08 01:38:16 +0700

    Show toast on TagManager initialization success
    Add success toast message when TagManager is ready.

--- ptuner/js/app.js
+++ ptuner/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     TagManager.init()
     .then(() => {
       console.log('TagManager is ready.');
+      showToast(`💾 <b>Database Ready</b><br>Loaded ${TagManager.getSize().toLocaleString()} tags successfully.`, 'info');
 
     })
     .catch(err => {
